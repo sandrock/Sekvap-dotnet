f339a0a99335eabf8ff17b168d942699330853f1
SandRock <sandrock@sandrock.fr>
2023-02-16 18:29:48 +0100

    Multi-framework build #3

--- src/SrkSekvap/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SrkSekvap portable-net45+sl5+win8+wp8+wpa81")]
-[assembly: AssemblyDescription(@"A simple Key/Value language ""Sekvap"" implemented for .NET. This is the PCL assembly.")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
